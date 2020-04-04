8b5198002ef3b4df5e9dd17bf2a84294b64cfba9
sshaw <skye.shaw@gmail.com>
2020-04-04 03:42:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# National Colors
+
+A JSON file containing each country's national colors, in the proper order.
+
+
+Scraped from https://en.wikipedia.org/wiki/National_colours which seems to be the only place
+that has the **correct colors** in the **correct order**.
+
+## Regenerating
+
+A small [Ruby](https://www.ruby-lang.org/en/) script is used to scrape the Wikipedia page. To run it,
+perform the following commands from this repository's root directory:
+
+    cd src
+    gem install normalize_country nokogiri
+    ruby extract.rb
+
+Alternatively, if you prefer [Bundler](https://bundler.io/):
+
+    cd src
+    bundle install
+    bundle exec ruby extract.rb
+
+The JSON will be sent to stdout.
+
+## html-colors
+
+Mapping of CSS colors to hex colors are done via a JSON file stolen from the
+[html-colors](https://github.com/radiovisual/html-colors) node package.
+
+## License
+
+Released under the MIT License: http://www.opensource.org/licenses/MIT
---
+++ src/extract.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require "open-uri"
+require "json"
+require "nokogiri"
+require "normalize_country"
+
+def to_hex(name)
+  # CSS names only
+  return name unless name =~ /\A[a-z]+\z/i
+  return unless HEX_COLOR[name.downcase]
+  HEX_COLOR[name.downcase]
+end
+
+def get_colors(styles)
+  colors = []
+  styles && styles.each do |s|
+    # match hex or CSS color name
+    next unless s["style"] =~ /background-color:\s*([#0-9a-zA-Z]+)/
+
+    value = $1
+    colors << to_hex(value)
+    abort "unknown color #{value}" unless colors[-1]
+  end
+
+  colors
+end
+
+HEX_COLOR = JSON.load(File.read(File.join(__dir__, "html-colors.json")))
+HEADINGS = ["Africa", "North America", "South America", "Asia", "Europe", "Oceania"].freeze
+RESULTS = []
+
+open("https://en.m.wikipedia.org/wiki/National_colours") do |io|
+  doc = Nokogiri::HTML(io)
+  doc.css("h2.section-heading").each do |node|
+    name = node.at(".mw-headline")
+    next unless name && HEADINGS.include?(name = name.text.strip)
+    next unless node.next && table = node.next.at("table")
+
+    # Skip table heading
+    table.css("tr")[1..-1].each do |tr|
+      next unless tr.children.any?
+
+      a = tr.children[0].at("a")
+      next unless a
+
+      country = a.text.strip
+
+      primary = get_colors(tr.css("td:nth-of-type(5) span[style]"))
+      secondary = get_colors(tr.css("td:nth-of-type(6) span[style]"))
+
+      abort "no colors found for #{country}" unless primary.any?
+
+      RESULTS << {
+        :country => country,
+        :alpha2  => NormalizeCountry(country, :to => :alpha2),
+        :colors => {
+          :primary => primary,
+          :secondary => secondary
+        }
+      }
+    end
+  end
+end
+
+puts JSON.pretty_generate(RESULTS)
